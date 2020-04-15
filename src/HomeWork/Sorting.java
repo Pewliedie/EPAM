d89854a22763f69573f0c1b3c590e44f360e6319
Whalesgohigh <56083057+Pewliedie@users.noreply.github.com>
2020-04-15 09:29:42 +0600

    Update Sorting.java

--- src/HomeWork/Sorting.java
+++ src/HomeWork/Sorting.java
@@ -14,29 +14,40 @@ public class Sorting {
         }
         //сортируем массив используя компаратор
         Collections.sort(sortedInts, new ReverseComparator());
+        Collections.sort(sortedInts,new ReverseComparator2());
         //Создаем лист из уникальных объектов
         Set<Integer> setOfSortedIntegers = new LinkedHashSet<>(sortedInts);
-        int count =0;
+        int count = 0;
         //Счет уникальных элементов
-        for (Integer ints:setOfSortedIntegers) {
+        for (Integer ints : setOfSortedIntegers) {
             count++;
         }
         System.out.println(setOfSortedIntegers);
-        System.out.println("Amount of unique elements is: "+count);
+        System.out.println("Amount of unique elements is: " + count);
     }
 }
 
+//
 class ReverseComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
         if (o2 == 4) {
             return 0;
-        }else if(o1==4) {
+        } else if (o1 == 4) {
             return -1;
         }
         if (o1 >= 100) {
-            return 1;
+            return o1-o2;
         }
         return o1 - o2;
+    }
+}
+class ReverseComparator2 implements Comparator<Integer>{
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if (o2<100){
+            return 1;
+        }
+        return o2-o1;
     }
 }
