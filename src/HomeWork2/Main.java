01e50f91d5fb8ff674ea274ebf8327cecb5117d5
Whalesgohigh <56083057+Pewliedie@users.noreply.github.com>
2020-04-21 15:40:19 +0600

    Home work 14.04.2020..Main

--- src/HomeWork2/Main.java
+++ src/HomeWork2/Main.java
@@ -2,12 +2,12 @@ package HomeWork2;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("Mercedes S63", "Black", "V8", "Spotcar", 90000,3199);
-        Car car2 = new Car("Audi Q7", "White", "V8", "Crossover", 80000,2341);
-        Car car3 = new Car("Toyota Camry XV50", "Gray", "V6", "Sedan ", 40000,4512);
-        Car car4 = new Car("Volvo XC90", "Gray", "V6", "SUV", 55000,9744);
-        Car car5 = new Car("Citroën ", "White", "V6", "Sedan", 44000,4511);
-        Car car6 = new Car("Koenigsegg Agera RS", "Black", "V8", "Sport Car", 3_000_000,7654);
+        Car car1 = new Car("Mercedes S63", "Black", "V8", "Spotcar", 90000, 3199);
+        Car car2 = new Car("Audi Q7", "White", "V8", "Crossover", 80000, 2341);
+        Car car3 = new Car("Toyota Camry XV50", "Gray", "V6", "Sedan ", 40000, 4512);
+        Car car4 = new Car("Volvo XC90", "Gray", "V6", "SUV", 55000, 9744);
+        Car car5 = new Car("Citroën ", "White", "V6", "Sedan", 44000, 4511);
+        Car car6 = new Car("Koenigsegg Agera RS", "Black", "V8", "Sport Car", 3_000_000, 7654);
 
         AutoShop autoShop = new AutoShop();
         autoShop.addCar(car1);
