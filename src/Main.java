04d97bb1ab4a648d1448c3c5f53f7f56d63c52c7
Whalesgohigh <56083057+Pewliedie@users.noreply.github.com>
2020-04-27 10:25:21 +0600

    Home work 27.04.2020

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Aquarium aquarium = new Aquarium();
+        boolean flag = true;
+        while (flag) {
+            System.out.println("Menu: Add, Remove, Change, Print all, Exit");
+            String input = scanner.nextLine();
+            switch (input.toLowerCase()) {
+                case "add":
+                    aquarium.addSpecies();
+                    break;
+                case "remove":
+                    aquarium.remove();
+                    break;
+                case "change":
+                    System.out.println("What do you want to change?: Age, Size");
+                    String change = scanner.nextLine();
+                    switch (change.toLowerCase()) {
+                        case "age":
+                            aquarium.changeAge();
+                            break;
+                        case "size":
+                            aquarium.changeSize();
+                            break;
+                    }
+                    break;
+                case "print all":
+                    aquarium.printAllSpecies();
+                    break;
+                case "exit":
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("There is no such action, please try again.");
+                    break;
+            }
+        }
+    }
+}
