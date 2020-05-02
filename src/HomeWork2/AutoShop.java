a1b8c1d6f67f67830920154ead67e22dabac571c
Whalesgohigh <56083057+Pewliedie@users.noreply.github.com>
2020-05-02 23:49:37 +0600

    Home work 02.05.2020

--- src/HomeWork2/AutoShop.java
+++ src/HomeWork2/AutoShop.java
@@ -2,11 +2,21 @@ package HomeWork2;
 
 import java.util.*;
 
-public class AutoShop {
-    private List<Car> listOfCarsForSale = new ArrayList<>();
+class AutoShop {
+    public List<Car> listOfCarsForSale = new ArrayList<>();
 
-    // добавление автомобился в лист для продажи
-    public void addCar(Car car) {
+    // добавление автомобился
+    public void addCar(Car car) throws OutOfFreePlaceException {
+
+        if (car.getHeight() > 300) {
+            throw new TooBigCarException(car.getMark() + " height exceeds the capacity, it cannot be more than 300cm");
+        } else if (car.getLength() > 500) {
+            throw new TooBigCarException(car.getMark() + " length exceeds the capacity, it cannot be more than 500cm");
+        } else if (car.getWidth() > 300) {
+            throw new TooBigCarException(car.getMark() + " width exceeds the capacity, it cannot be more than 300cm");
+        } else if (listOfCarsForSale.size() >= 5) {
+            throw new OutOfFreePlaceException("You cannot add more than 5 cars");
+        }
         listOfCarsForSale.add(car);
     }
 
--- src/HomeWork2/Main.java
+++ src/HomeWork2/Main.java
@@ -2,27 +2,23 @@ package HomeWork2;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("Mercedes S63", "Black", "V8", "Spotcar", 90000, 3199);
-        Car car2 = new Car("Audi Q7", "White", "V8", "Crossover", 80000, 2341);
-        Car car3 = new Car("Toyota Camry XV50", "Gray", "V6", "Sedan ", 40000, 4512);
-        Car car4 = new Car("Volvo XC90", "Gray", "V6", "SUV", 55000, 9744);
-        Car car5 = new Car("Citroën C4", "White", "V6", "Sedan", 44000, 4511);
-        Car car6 = new Car("Koenigsegg Agera RS", "Black", "V8", "Sport Car", 3_000_000, 7654);
+        Car car1 = new Car("Mercedes S63", 30000, 200, 200, 200);
+        Car car2 = new Car("Audi Q7", 40000, 300, 500, 200);
+        Car car3 = new Car("Toyota Camry XV50", 20000, 250, 250, 200);
+        Car car4 = new Car("Volvo XC90", 30000, 250, 250, 250);
+        Car car5 = new Car("Citroën C4", 60000, 250, 250, 250);
+        Car car6 = new Car("Koenigsegg Agera RS", 40000, 250, 450, 299);
 
         AutoShop autoShop = new AutoShop();
-        autoShop.addCar(car1);
-        autoShop.addCar(car2);
-        autoShop.addCar(car3);
-        autoShop.addCar(car4);
-        autoShop.addCar(car5);
-        autoShop.addCar(car6);
-
-        autoShop.showPriceAllAvailableCars();
-        System.out.println();
-
-        autoShop.buyCar(car1);
-
-        System.out.println();
-        autoShop.showPriceAllAvailableCars();
+        try {
+            autoShop.addCar(car1);
+            autoShop.addCar(car2);
+            autoShop.addCar(car3);
+            autoShop.addCar(car4);
+            autoShop.addCar(car5);
+            autoShop.addCar(car6);
+        } catch (TooBigCarException | OutOfFreePlaceException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
